88fc585162d32efdff78e5b781ce3b18d16db0d4
Danysikazwe1000 <165155739+Danysikazwe1000@users.noreply.github.com>
2025-05-27 12:10:39 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 // Contact details
-global.ownernomer = process.env.OWNER_NOMER || "2348109263390";
-global.ytname = process.env.YT_NAME || "YT: @EliteProTech";
-global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
+global.ownernomer = process.env.OWNER_NOMER || "255672562733";
+global.ytname = process.env.YT_NAME || "YT: @Viral";
+global.socialm = process.env.SOCIAL_M || "GitHub: Dc";
 global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
 
 // Creator details
-global.ownernumber = process.env.OWNER_NUMBER || '2348109263390';
-global.ownername = process.env.OWNER_NAME || 'ElitePro';
-global.botname = process.env.BOT_NAME || 'ELITE-PRO-V1';
+global.ownernumber = process.env.OWNER_NUMBER || '255672562733';
+global.ownername = process.env.OWNER_NAME || 'Viral';
+global.botname = process.env.BOT_NAME || 'Viral';
 
 // Default settings 
 global.prefa = process.env.PREFIX ? process.env.PREFIX.split(',') : ['', '!', '.', '#', '&'];
@@ -22,7 +22,7 @@ global.autorecordtype = process.env.AUTO_RECORD_TYPE === 'true';
 global.autoread = process.env.AUTO_READ === 'true';
 global.autobio = process.env.AUTO_BIO !== 'false'; // default true
 global.anti92 = process.env.ANTI_92 === 'true';
-global.autoswview = process.env.AUTO_SW_VIEW !== 'false'; // default true
+global.autoswview = process.env.AUTO_SW_VIEW !== 'true'; // default true
 global.welcome = process.env.WELCOME !== 'false'; // default true
 global.autoreact = process.env.AUTO_REACT === 'true';
 global.autolikestatus = process.env.AUTO_LIKE_STATUS === 'true';
